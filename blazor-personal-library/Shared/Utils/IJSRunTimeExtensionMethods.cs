2fbbb41bbcdb4a05b8d0ae1b91bbf97c7b47eb26
Unai <unaidevcoding@gmail.com>
2026-05-11 14:35:18 +0200

    add confirm alert to delete book

---
+++ blazor-personal-library/Shared/Utils/IJSRunTimeExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace blazor_personal_library.Shared.Utils
+{
+    public static class IJSRunTimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime jSRuntime, string message)
+        {
+            return await jSRuntime.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
